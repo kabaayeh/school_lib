cf5bb2c90e124386d8fc7bed00738e9d0c0c8089
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-12-29 07:38:48 +0000

    Move the menu method to the main.rb

--- app.rb
+++ app.rb
@@ -12,36 +12,6 @@ class App
     @rentals = []
   end
 
-  def run
-    puts menu
-  end
-
-  # App Menu
-  def menu
-    puts 'Welcome to School Library App!'
-    loop do
-      puts ''
-      puts 'Please choose an option by entering a number:'
-      puts '1 - List all books'
-      puts '2 - List all people'
-      puts '3 - Create a person'
-      puts '4 - Create a book'
-      puts '5 - Create a rental'
-      puts '6 - List all rentals for a given person id'
-      puts '7 - Exit'
-      puts ''
-      user_input = gets.chomp.to_i
-      puts ''
-
-      case user_input
-      when 1..6 then run_option(user_input)
-      when 7 then puts 'Thank you for using the app!'
-      else puts 'Please enter valid option'
-      end
-      break unless user_input != 7
-    end
-  end
-
   def run_option(user_input)
     case user_input
     when 1
--- main.rb
+++ main.rb
@@ -1,8 +1,34 @@
 require_relative 'app'
 
 def main
-  app = App.new
-  app.run
+  puts menu
+end
+
+ # App Menu
+ def menu
+  puts 'Welcome to School Library App!'
+  loop do
+    puts ''
+    puts 'Please choose an option by entering a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+    puts ''
+    user_input = gets.chomp.to_i
+    puts ''
+
+    app = App.new
+    case user_input
+    when 1..6 then app.run_option(user_input)
+    when 7 then puts 'Thank you for using the app!'
+    else puts 'Please enter valid option'
+    end
+    break unless user_input != 7
+  end
 end
 
 main
